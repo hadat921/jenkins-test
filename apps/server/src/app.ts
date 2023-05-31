0faf35351198a2c37a8019e24da8b45bb920bfdb
Ha Gia Dat [Edric] <Edric@nexondv.com>
2023-05-31 09:40:11 +0700

    test jenkins

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,19 +1,19 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { Router } from 'express';
-import { IRoutes } from './interfaces/routes';
-import cors from 'cors'
-import passport from 'passport';
-import './middlewares/auth'
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { Router } from "express";
+import { IRoutes } from "./interfaces/routes";
+import cors from "cors";
+import passport from "passport";
+import "./middlewares/auth";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/test', (req: Request, res: Response) => {
-  res.send('Done!!!!!!!!!');
+app.get("/test", (req: Request, res: Response) => {
+  res.send("Done rui ne!!!!!!!!!");
 });
-app.use(Router)
+app.use(Router);
 class App {
   public app: express.Application;
   public port: string | number;
@@ -22,7 +22,7 @@ class App {
   constructor(routes: IRoutes[]) {
     this.app = express();
     this.port = process.env.PORT || 3000;
-    this.env = process.env.NODE_ENV || 'development';
+    this.env = process.env.NODE_ENV || "development";
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
@@ -33,14 +33,13 @@ class App {
       console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
       console.log(`⚡️[server]: DB connect successfully ${this.env}`);
     });
-    
   }
 
   public getServer() {
     return this.app;
   }
 
-  private initializeMiddlewares() { 
+  private initializeMiddlewares() {
     this.app.use(cors());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
@@ -48,12 +47,10 @@ class App {
   }
 
   private initializeRoutes(routes: IRoutes[]) {
-    routes.forEach(route => {    
+    routes.forEach((route) => {
       this.app.use(route.path, route.router);
     });
   }
 }
 
 export default App;
-
-
